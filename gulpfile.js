0c25a1cf9089c5c694bb877c3614e6edbddfe3d9
romaskirman <romanskirman@gmail.com>
2022-02-08 19:40:34 +0300

    feat: gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,42 @@
 const {src, dest, parallel, series, watch} = require('gulp');
 const browserSync = require('browser-sync');
-// const autoprefixer = require('gulp-autoprefixer');
-// const cleanCSS = require('gulp-clean-css');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
 const del = require('del');
 const imagemin = require('gulp-imagemin');
 const sass = require('gulp-sass')(require('sass'));
 
-// function browsersync() {
-//     browserSync.init({
-//         server: {
-//             baseDir: 'build'
-//         }
-//     })
-// }
+function browsersync() {
+    browserSync.init({
+        server: {
+            baseDir: 'build'
+        }
+    })
+}
 
 function html() {
     return src('src/index.html')
         .pipe(dest('build'))
-    // .pipe(browserSync.stream())
+    .pipe(browserSync.stream())
 }
 
 function css() {
-    return src('src/assets/styles/style.css')
-        //.pipe(sass().on('error', sass.logError))
-        // .pipe(autoprefixer({
-        //     overrideBrowserslist: ['last 2 versions'],
-        //     grid: 'autoplace',
-        // }))
-        // .pipe(cleanCSS())
+    return src('src/assets/styles/style.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            overrideBrowserslist: ['last 2 versions'],
+            grid: 'autoplace',
+        }))
+        .pipe(cleanCSS())
         .pipe(dest('build/assets/styles'))
-        //.pipe(browserSync.stream())
+        .pipe(browserSync.stream())
 }
 
 function images() {
     return src('src/assets/images/**/*')
         .pipe(imagemin())
         .pipe(dest('build/assets/images'))
-        //.pipe(browserSync.stream())
+        .pipe(browserSync.stream())
 }
 
 function fonts() {
@@ -50,14 +50,14 @@ function clear() {
 }
 
 
-// function startWatch() {
-//     watch('src/index.html', html)
-//     watch('src/assets/styles/**/*.scss', css)
-//     watch('src/assets/images/**/*', images)
-//     watch('src/assets/fonts/**/*', fonts)
-// }
+function startWatch() {
+    watch('src/index.html', html)
+    watch('src/assets/styles/**/*.scss', css)
+    watch('src/assets/images/**/*', images)
+    watch('src/assets/fonts/**/*', fonts)
+}
 
-exports.dev = parallel(html, images, fonts, css)
+exports.dev = parallel(browsersync, startWatch, html, images, fonts, css)
 exports.build = series(clear, parallel(html, images, fonts, css))
 
 
